04fd97ef06d12a8fb5dee5082bde8fde2e29be06
krishnasairammalapaka <krishnasairammalapaka@gmail.com>
2025-03-23 11:25:03 +0530

    types of datatypes

---
+++ datatypes.java
@@ -0,0 +1,24 @@
+class datatypes {
+    public static void main(String a[])
+    {
+        //integer
+        int num = 10;
+        byte b = 20;
+        short s = 30;
+        long l = 40l;
+        //float
+        float f = 12.34f;
+        double d = 12.3456789;
+        //character
+        char c = 'A';
+        //boolean
+        boolean bool = true;
+        //string
+        String str = "Hello";
+        //object
+        Object obj = new Object();
+        //array
+        int[] arr = {1, 2, 3, 4, 5};
+       
+    }
+}
