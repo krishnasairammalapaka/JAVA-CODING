1f40405cec335a6fec93c642c07f82b4105b0cb6
krishnasairammalapaka <krishnasairammalapaka@gmail.com>
2025-03-23 12:04:15 +0530

    arithmetic operations

---
+++ arithmeticoperations.java
@@ -0,0 +1,14 @@
+public class arithmeticoperations {
+   public static void main(String a[])
+   {
+    int num1 = 10;
+    // int num2 = 20;
+    // int result = num1 + num2;
+    // System.out.println(result);
+    num1++;                     //post increment
+    ++num1;                     //pre increment 
+    System.out.println(num1);
+   
+
+   }
+}
