9c9e82a892605a8c7cd9187a2d9c98e30d5bbd29
krishnasairammalapaka <krishnasairammalapaka@gmail.com>
2025-03-23 11:37:02 +0530

    added the literls

--- literals.java
+++ literals.java
@@ -1,8 +1,17 @@
-class literals {
+// class literals {
+//     public static void main(String a[])
+//     {
+//         int num = 0b101;
+//         num++;
+//         System.out.println(num);
+//     }
+// }
+public class literals{
+
     public static void main(String a[])
     {
         int num = 10;
-        num++;
-        System.out.println(a);
+        num ++;
+        System.out.println(num);
     }
 }
