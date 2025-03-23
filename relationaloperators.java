a35e37dafb0449e38d2a93e77d840d4182bc294b
krishnasairammalapaka <krishnasairammalapaka@gmail.com>
2025-03-23 12:11:41 +0530

    relational operators in java

---
+++ relationaloperators.java
@@ -0,0 +1,22 @@
+public class relationaloperators {
+    public static void main(String a[])
+    {
+        int num1 = 10;
+        int num2 = 20;
+        // boolean result = num1<num2;
+        // System.out.println(result);
+
+        
+        // boolean result = num1 > num2;
+        // System.out.println(result);
+        
+
+
+        // boolean result = num1==num2;
+        // System.out.println(result);
+
+
+        boolean result = num1!=num2;
+        System.out.println(result);
+    }
+}
