8c09f722a440620f7c1f1b284bc287a0d7fb62b1
krishnasairammalapaka <krishnasairammalapaka@gmail.com>
2025-03-23 13:10:03 +0530

    conditional statements

---
+++ conditionalstatemets.java
@@ -0,0 +1,11 @@
+public class conditionalstatemets {
+    public static void main(String[] args)
+    {
+        int x = 100;
+
+        if (x>234)
+            System.out.println("hellow");
+        else
+            System.out.println("get the fuck out of here");
+    }
+}
