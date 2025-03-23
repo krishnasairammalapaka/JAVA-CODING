5adb81537668f31274fb3294f8dd8c9af8dee62e
krishnasairammalapaka <krishnasairammalapaka@gmail.com>
2025-03-23 11:08:55 +0530

    adding of two numbers using variables

---
+++ adding.java
@@ -0,0 +1,9 @@
+public class adding {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 9;
+        int result = a+b;
+        System.out.println(result);
+    }
+}
+
