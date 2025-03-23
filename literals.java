e536af770ef514818036d72baa83407e62a67613
krishnasairammalapaka <krishnasairammalapaka@gmail.com>
2025-03-23 11:37:48 +0530

    full laters

--- literals.java
+++ literals.java
@@ -1,11 +1,11 @@
-// class literals {
-//     public static void main(String a[])
-//     {
-//         int num = 0b101;
-//         num++;
-//         System.out.println(num);
-//     }
-// }
+class literals {
+    public static void main(String a[])
+    {
+        int num = 0b101;
+        num++;
+        System.out.println(num);
+    }
+}
 public class literals{
 
     public static void main(String a[])
